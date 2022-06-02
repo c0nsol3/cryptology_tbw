3dc59e2c58a95016f9d46eb98cac8d1d05047afa
c0nsol3 <consol3@protonmail.com>
2022-06-02 22:49:22 +0200

    fix queries

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -60,7 +60,10 @@ export const getCurrentVotersSince = (
     transactions."asset", transactions."timestamp" \
     FROM transactions \
     WHERE transactions."type" = 3 AND transactions."type_group" = 1 \
-    AND (transactions."asset"->>'votes' LIKE '%+${delegatePublicKey}%' OR transactions."asset"->>'votes' LIKE '%+${delegateName}%') \
+    AND ( \
+        transactions."asset" @> '{"votes": ["+${delegatePublicKey}"]}' \
+        OR transactions."asset" @> '{"votes": ["+${delegateName}"]}' \
+    )\
     ORDER BY transactions."sender_public_key", transactions."timestamp" DESC ) t \
     ORDER BY t."timestamp" DESC;`;
 };
@@ -79,7 +82,12 @@ export const getVoterSinceHeight = (
           blocks."height" \
           FROM transactions INNER JOIN blocks ON blocks."id" = transactions."block_id"
           WHERE transactions."type" = 3 AND transactions."type_group" = 1 \
-          AND (transactions."asset"->>'votes' LIKE '%${delegatePublicKey}%' OR transactions."asset"->>'votes' LIKE '%${delegateName}%') \
+          AND ( \
+              transactions."asset" @> '{"votes": ["+${delegatePublicKey}"]}' \
+              OR transactions."asset" @> '{"votes": ["-${delegatePublicKey}"]}' \
+              OR transactions."asset"  @> '{"votes": ["+${delegateName}"]}' \
+              OR transactions."asset"  @> '{"votes": ["-${delegateName}"]}' \
+            ) \
           AND blocks.height >= ${startBlockHeight} ORDER BY blocks."height" ASC;`;
 };
 
